9230de082ab8a1e3aea0797d8c75dafe1ad1329e
Alejandro González <90796690+algonz39@users.noreply.github.com>
2023-10-05 20:18:37 +0200

    Ya no peta

--- skeleton/Clases/ParticleGenerator.cpp
+++ skeleton/Clases/ParticleGenerator.cpp
@@ -1,22 +1,19 @@
 #include "ParticleGenerator.h"
 #include <cmath>
 
-ParticleGenerator::ParticleGenerator()
-{
+ParticleGenerator::ParticleGenerator() {
 	nParticles = 0;
 	cam = GetCamera();
 }
 
-ParticleGenerator::~ParticleGenerator()
-{
+ParticleGenerator::~ParticleGenerator() {
 	for (Particle* p : particles) {
-		delete p;
+		if(p) delete p;
 	}
 	particles.clear();
 }
 
-void ParticleGenerator::generateProjectile(ProjectileType type)
-{
+void ParticleGenerator::generateProjectile(ProjectileType type) {
 	Vector3 pos = cam->getEye();
 	Vector3 vel = cam->getDir();
 	Vector3 grav = { 0, -1, 0 };
@@ -33,7 +30,7 @@ void ParticleGenerator::generateProjectile(ProjectileType type)
 		projectile = new Particle(pos, vel * 10.0, grav * -0.6, 1, { 1, 0, 0, 1 });
 		break;
 	case Laser:
-		projectile = new Particle(pos, vel * 40.0, grav * 0, 0.1, { 0.5, 0.5, 0.5, 1 });
+		projectile = new Particle(pos, vel * 40.0, grav * 0, 0.1, { 1, 1, 0, 1 });
 		break;
 	}
 	particles.push_back(projectile);
@@ -41,14 +38,19 @@ void ParticleGenerator::generateProjectile(ProjectileType type)
 
 }
 
-bool isDead(Particle* p) { return p->isDead(); }
-
 void ParticleGenerator::updateParticles(double t)
 {
-	for (Particle* p : particles) {
+	for (auto iter = particles.begin(); iter != particles.end();) {
+		Particle* p = *iter;
 		p->integrate(t);
+
+		if (p->isDead()) {
+			delete p;
+			iter = particles.erase(iter);
+		}
+		else {
+			++iter;  // Avanza el iterador si el objeto no está muerto
+		}
 	}
-	
-	std::remove_if(particles.begin(), particles.end(), isDead);
 }
 
